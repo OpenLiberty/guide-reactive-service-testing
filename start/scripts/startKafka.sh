c61e704bf29064549c9d23e95b2f1216d9b8c4f7
Ruilin Ma <Ruilin.Ma@ibm.com>
2023-11-17 10:57:18 -0500

    supporting devc mode

---
+++ start/scripts/startKafka.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NETWORK=reactive-app
+
+docker network create $NETWORK
+
+docker run -d \
+    -e ALLOW_PLAINTEXT_LISTENER=yes \
+    -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 \
+    -e KAFKA_CFG_NODE_ID=0 \
+    -e KAFKA_CFG_PROCESS_ROLES=controller,broker \
+    -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092,CONTROLLER://:9093 \
+    -e KAFKA_CFG_CONTROLLER_QUORUM_VOTERS=0@kafka:9093 \
+    -e KAFKA_CFG_CONTROLLER_LISTENER_NAMES=CONTROLLER \
+    --hostname=kafka \
+    --network=$NETWORK \
+    --name=kafka \
+    --rm \
+    bitnami/kafka:latest &
