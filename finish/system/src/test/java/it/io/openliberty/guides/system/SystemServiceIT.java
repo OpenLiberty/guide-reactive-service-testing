578a5d2e143191324b0c9c3ea297238abc510a1e
Ruilin Ma <this.is.millerma@gmail.com>
2024-01-24 15:41:36 -0500

    improve consumer hotspot

--- finish/system/src/test/java/it/io/openliberty/guides/system/SystemServiceIT.java
+++ finish/system/src/test/java/it/io/openliberty/guides/system/SystemServiceIT.java
@@ -52,9 +52,11 @@ public class SystemServiceIT {
     private static Logger logger = LoggerFactory.getLogger(SystemServiceIT.class);
 
     private static Network network = Network.newNetwork();
+    
     // tag::KafkaConsumer2[]
     public static KafkaConsumer<String, SystemLoad> consumer;
     // end::KafkaConsumer2[]
+
     // tag::buildSystemImage[]
     private static ImageFromDockerfile systemImage
         = new ImageFromDockerfile("system:1.0-SNAPSHOT")
